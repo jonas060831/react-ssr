5ddf352c797900a2131f91446c36623ac7d6503d
jonas060831 <jonas_sulit2001@yahoo.com>
2021-07-22 03:11:42 -0700

    webpack updates

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Home = () => {
-    return <div>I'm the home component</div>;
+    return (
+        <div>
+            I'm the home component <br />
+            <button onClick={ () => console.log('Hi There')} >Press me!</button>
+        </div>);
 };
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
-const express = require('express');
-const React = require('react');
-const renderToString = require('react-dom/server').renderToString;
-require('dotenv').config()
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import dotenv, { config } from 'dotenv';
+
+import Home from './client/components/Home';
+
+import lanipaddress from '../lanipaddress';
 
 const app = express();
 
-const Home = require('./client/components/Home').default;
-
 const PORT = process.env.REACT_APP_PORT || 3000;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 
     const content = renderToString(<Home/>);
 
+    const html = `
+        <html>
+            <head></head>
+            <body>
+              <div>${content}</div>
+              <script src="bundle.js"></script>  
+            </body>
+        </html>
+    `;
 
-    res.send(content)
+    res.send(html)
 })
 
-
-app.listen( PORT, () => {
-	console.log(`Listening on PORT ${PORT}`)
+app.listen( PORT, (req) => {
+    console.log('\n')
+	console.log(`Local: \t \t http://localhost:${PORT}`)
+    console.log(`On Your Network: http://${lanipaddress}:${PORT}`)
 });
