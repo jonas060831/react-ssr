b85dfc92f4b3bb8d842bbbc85f860d1be35e9308
jonas060831 <jonas_sulit2001@yahoo.com>
2021-07-22 04:12:27 -0700

    merge all scripts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
 import dotenv, { config } from 'dotenv';
 
-import Home from './client/components/Home';
+import renderer from './helpers/renderer';
 
 import lanipaddress from '../lanipaddress';
 
@@ -14,24 +12,11 @@ const PORT = process.env.REACT_APP_PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-
-    const content = renderToString(<Home/>);
-
-    const html = `
-        <html>
-            <head></head>
-            <body>
-              <div>${content}</div>
-              <script src="bundle.js"></script>  
-            </body>
-        </html>
-    `;
-
-    res.send(html)
+    res.send(renderer())
 })
 
 app.listen( PORT, (req) => {
     console.log('\n')
 	console.log(`Local: \t \t http://localhost:${PORT}`)
-    console.log(`On Your Network: http://${lanipaddress}:${PORT}`)
+    console.log(`On Your Network: http://${lanipaddress}:${PORT} \n`)
 });
